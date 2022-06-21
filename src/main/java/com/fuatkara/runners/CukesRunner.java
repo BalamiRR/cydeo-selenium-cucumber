441c6d9e0764ccc4ddeb7db01eebd5208e91f1bd
Fuat Kara -                                 Junior Java Developer <36539306+BalamiRR@users.noreply.github.com>
2022-06-21 20:21:20 +0200

    cucumber-junit

---
+++ src/main/java/com/fuatkara/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.fuatkara.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
+        features = "src/main/resources/features",
+        glue = "com/fuatkara/step_definitions",
+        dryRun = false,
+        //tags="@employee"  //sadece employee icerenleri gosterir
+        //tags="@employee and @admin"   //or da diyebiliriz. o zaman hepsini gosterir
+        //tags="@Regression and not @student"
+        tags="@Regression"
+
+)
+public class CukesRunner {
+
+
+}
