41a075e7698b683bf914debd5f3dbbf9a4e8372d
Fuat Kara -                                 Junior Java Developer <36539306+BalamiRR@users.noreply.github.com>
2022-07-02 15:54:51 +0200

    last update

--- src/main/java/com/fuatkara/runners/CukesRunner.java
+++ src/main/java/com/fuatkara/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/main/resources/features",
         glue = "com/fuatkara/step_definitions",
         dryRun = false, // if we dont want to open our browser, so write true
-        tags="@femaleScientists or @soccerPlayers"
+        tags=""
         //tags="@employee"  //sadece employee icerenleri gosterir
         //tags="@employee and @admin"   //or da diyebiliriz. o zaman hepsini gosterir
         //tags="@Regression and not @student"
