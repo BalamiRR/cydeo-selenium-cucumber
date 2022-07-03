6efef820ff38f23046e6ee6689f0f8fa520e0ca3
Fuat Kara -                                 Junior Java Developer <36539306+BalamiRR@users.noreply.github.com>
2022-07-03 13:26:06 +0200

    cukesRunner and Hooks updates

--- src/main/java/com/fuatkara/step_definitions/Hooks.java
+++ src/main/java/com/fuatkara/step_definitions/Hooks.java
@@ -14,20 +14,20 @@ public class Hooks {
     //import org.junit.After;
     //import org.junit.Before;
 
-    @Before(order = 1)
-    public void setupScenario(){
-        System.out.println("===Setting up browser using cucumber @Before");
-    }
+    //@Before(order = 1)
+    //public void setupScenario(){
+    //    System.out.println("===Setting up browser using cucumber @Before");
+    //}
 
-    @Before(value="@login", order=2)
-    public void setupScenarioForLogins(){
-        System.out.println("===This will only apply to scenarios with @login tag");
-    }
+    //@Before(value="@login", order=2)
+    //public void setupScenarioForLogins(){
+    //    System.out.println("===This will only apply to scenarios with @login tag");
+    //}
 
-    @Before(value="@db", order=0)
-    public void setupForDatabaseScenarios(){
-        System.out.println("===This will only apply to scenarios with @db tag");
-    }
+    //@Before(value="@db", order=0)
+    //public void setupForDatabaseScenarios(){
+    //    System.out.println("===This will only apply to scenarios with @db tag");
+    //}
 
     @After
     public void teardownScenario(Scenario scenario){
@@ -45,15 +45,15 @@ public class Hooks {
         //System.out.println("===Scenario ended/ Take screenshot if failed!");
     }
 
-    @BeforeStep
-    public void setupStep(){
-        System.out.println("-----> applying setup using @BeforeStep");
-    }
+    //@BeforeStep
+    //    public void setupStep(){
+    //        System.out.println("-----> applying setup using @BeforeStep");
+    //    }
 
-    @AfterStep
-    public void afterStep(){
-        System.out.println("-----> applying tearDown using @AfterStep");
-    }
+    //@AfterStep
+    //    public void afterStep(){
+    //        System.out.println("-----> applying tearDown using @AfterStep");
+    //    }
 
 }
 
